54cbf6a53793fe6decd2537474639196468772d9
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2022-12-07 13:34:44 +0300

    feat: add burger menu function

--- lucky-test/index.js
+++ lucky-test/index.js
@@ -1,3 +1,5 @@
+//validate search input
+
 const form = document.querySelector(".hero__search-form");
 const searchInput = document.querySelector(".hero__search-input");
 const tooltip = document.querySelector(".hero__tooltip");
@@ -20,31 +22,44 @@ searchInput.addEventListener("input", function () {
   }
 });
 
+//animate flags
+let flagsCircle = document.querySelector(".hero__animation-circle");
+let flags = Array.from(document.getElementsByClassName("hero__animation-flag"));
+const body = document.querySelector("body");
+document.addEventListener("DOMContentLoaded", function () {
+  flags.forEach(function (flag, index) {
+    setTimeout(function () {
+      timer(flag, index);
+    }, 400 * ++index);
+  });
 
-let flagsCircle = document.querySelector('.hero__animation-circle')
-let flags = Array.from(document.getElementsByClassName('hero__animation-flag'));
-const body = document.querySelector('body')
-document.addEventListener("DOMContentLoaded", function(){
-
-flags.forEach(function(flag, index){
-    setTimeout(function(){
-       timer(flag, index);
-    }, 400 * ++index)
- });
-
-setTimeout(function(){
+  setTimeout(function () {
     infinite();
- }, 4000);
-})
+  }, 4000);
+});
 
-function infinite(){
-    flagsCircle.classList.add('infinite')
+function infinite() {
+  flagsCircle.classList.add("infinite");
 }
 
-function timer(flag, index){
-    flag.classList.remove('center-position')
-    flag.classList.add(`flag-${index}`)
-
+function timer(flag, index) {
+  flag.classList.remove("center-position");
+  flag.classList.add(`flag-${index}`);
 }
 
+// toggle open menu
+const burger = document.querySelector(".header__burger");
+const headerNav = document.querySelector(".header__nav");
+const navList = document.querySelector(".header__nav_list");
+let isMenuOpen = false;
+burger.addEventListener("click", toggleMenu);
 
+const menuItems = Array.from(document.querySelectorAll(".header__nav_item"));
+menuItems.forEach((item) => item.addEventListener("click", toggleMenu));
+function toggleMenu() {
+  burger.classList.toggle("is-active");
+  burger.classList.toggle("burger-x");
+  headerNav.classList.toggle("open");
+  headerNav.style.height = isMenuOpen ? "0px" : navList.offsetHeight + "px";
+  isMenuOpen = !isMenuOpen;
+}
